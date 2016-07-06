2a5fecb8f03e1c174f46a7a1fa563d1b884a2282
Colin Delhalle <delhalle.colin@gmail.com>
2016-07-06 21:24:35 +0200

    ajout projet test unitaire

---
+++ test/RecettesControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace apis
+{
+    public class RecetteControllerTest
+    {
+        [Fact]
+        public void UniTest1()
+        {
+            Assert.True(true);
+        }
+    }
+}
